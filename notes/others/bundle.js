b948a726ed151601eb06934e6967aeeacd35b126
sohoorc <me@gwtcoder.me>
2018-08-16 23:56:30 +0800

    add others

---
+++ notes/others/bundle.js
@@ -0,0 +1,37 @@
+var fs = require('fs');
+var archiver = require('archiver');
+
+function bundleCompress(path) {
+  fs.exists('bundles', function (exist) {
+    if (!exist) {
+      fs.mkdir('bundles', function () {
+        bundle(path)
+      })
+    }else{
+      bundle(path)
+    }
+  })
+
+}
+
+function bundle(path){
+  let date =  new Date();
+  let hours = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+
+  let fileName = 'XXXX系统-'+date.toLocaleDateString().replace(/\-/g,'')+hours+min+sec;
+  let output = fs.createWriteStream('./bundles/' + fileName + '.zip');
+  let archive = archiver('zip', {
+    zlib: { level: 9 } // Sets the compression level.
+  });
+
+  // pipe archive data to the file
+  archive.pipe(output);
+
+  archive.directory(path, fileName);
+  // archive.directory(path, 'new-subdir');
+  archive.finalize();
+}
+
+module.exports = bundleCompress
