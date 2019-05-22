51fa3b1412137c11fc307886ac87b6af2bedf8dc
sohoorc <me@gwtcoder.me>
2019-05-22 10:22:25 +0800

    添加imgResize

---
+++ notes/others/imgResize.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const gm = require('gm');
+const fs = require('fs');
+
+const imgHanding = function (imgPath, outputPath) {
+  gm(imgPath).resize(375).write(outputPath,(err,res)=>{
+    if(err) throw err;
+  })
+}
+
+const getFileDir = function (inputPath, outputPath) {
+  fs.readdir(inputPath, (err, files) => {
+    if (err) return err;
+    files.forEach((fileName, index) => {
+      const extname = path.extname(fileName)
+      const regImgExt = /.png|.jpg|.jpeg/
+      if (extname.match(regImgExt)) {
+        const imgPath = path.resolve(inputPath) + '/' + fileName
+        const optDir = path.resolve(outputPath)+ '/' +fileName
+        imgHanding(imgPath, optDir)
+      }
+    })
+
+  })
+}
+
+const inputPath = './static/input/'
+const outputPath = './static/output/'
+
+getFileDir(inputPath,outputPath)
